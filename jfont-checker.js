918903a35ba2e2dca1e045cf2d03ed1b7a23dde6
Derek Leung <derek1906@gmail.com>
2015-01-27 03:32:28 -0600

    First commit

---
+++ jfont-checker.js
@@ -0,0 +1,82 @@
+﻿﻿/**
+*
+*  JFont Checker
+*  Derek Leung
+*  Original Date: 2010.8.23
+*  Current: Jan, 2015
+*  
+*  This piece of code checks for the existence of a specified font.
+*  It ultilizes the font fallback mechanism in CSS for font checking.
+*  
+*  Compatibility:
+*  Tested on Chrome, Firefox, IE6+
+*  Requires CSS and JS
+*  
+**/
+(function(){
+	var containerA, containerB, body = document.body,
+		filler = "random_words_#_!@#$^&*()_+mdvejreu_RANDOM_WORDS";
+
+	function createContainers(){
+		containerA = document.createElement("span");
+		containerB = document.createElement("span");
+
+		containerA.innerText = filler;
+		containerB.innerText = filler;
+
+		var styles = {
+			margin: "0",
+			padding: "0",
+			fontSize: "32px",
+			position: "absolute",
+			zIndex: "-1"
+		};
+
+		for(var key in styles){
+			if(styles.hasOwnProperty(key)){
+				containerA.style[key] = styles[key];
+				containerB.style[key] = styles[key];
+			}
+		}
+
+		return function(){
+			//clean up
+			containerA.outerHTML = "";
+			containerB.outerHTML = "";
+		};
+	}
+
+	function checkDimension(){
+		return containerA.offsetWidth === containerB.offsetWidth &&
+			   containerA.offsetHeight === containerB.offsetHeight;
+	}
+
+	function checkfont(font){
+		var result = null,
+			reg = /[\,\.\/\;\'\[\]\`\<\>\\\?\:\"\{\}\|\~\!\@\#\$\%\^\&\*\(\)\-\=\_\+]/g,
+			cleanUp = createContainers();
+
+		font = font.replace(reg, "");
+
+		body.appendChild(containerA);
+		body.appendChild(containerB);
+
+		//First Check
+		containerA.style.fontFamily = font + ",monospace";
+		containerB.style.fontFamily = "monospace";
+
+		if(checkDimension()){
+		   	//Assume Arial exists, Second Check
+			containerA.style.fontFamily = font + ",Arial";
+			containerB.style.fontFamily = "Arial";
+			result = !checkDimension();
+		}else{
+			result = true;
+		}
+
+		cleanUp();
+		return result
+	}
+
+	this.checkfont = checkfont;
+})();
